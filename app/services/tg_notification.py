74dfa89f721e6efa495174b6fd58b6daa80253a9
Fedor <Fedor-babunov@mail.ru>
2026-06-19 08:29:03 +0300

    fix: обработка всех ошибок Telegram при отправке

--- app/services/tg_notification.py
+++ app/services/tg_notification.py
@@ -1,5 +1,5 @@
-import aiogram.exceptions
 from aiogram import Bot
+from aiogram.exceptions import TelegramAPIError
 
 from app.config.config import config, logger
 
@@ -8,5 +8,5 @@ async def send_all_users(msg: str, users: list[int] = config.USERS, bot: Bot = c
     for user in users:
         try:
             await bot.send_message(user, msg)
-        except aiogram.exceptions.TelegramBadRequest as e:
+        except TelegramAPIError as e:
             logger.error(f"Ошибка отправки пользователю {user}: {e}")
